7f9e7c82812461a55ddb8bddbd2b68fe77d486b5
IrisCode <150204523+frank777ch@users.noreply.github.com>
2024-07-08 16:27:55 -0500

    Fixing backend test

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,21 @@ const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// Registro de usuario
 exports.register = async (req, res) => {
     const { firstName, lastName, email, password, birthDate, phone, address, gender, school } = req.body;
     try {
-        const user = new User({ firstName, lastName, email, password, birthDate, phone, address, gender, school });
+        const user = new User({
+            firstName: firstName.toString(),
+            lastName: lastName.toString(),
+            email: email.toString(),
+            password: password.toString(),
+            birthDate: new Date(birthDate),
+            phone: phone.toString(),
+            address: address.toString(),
+            gender: gender.toString(),
+            school: school.toString()
+        });
         await user.save();
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(201).json({ token });
@@ -14,14 +25,15 @@ exports.register = async (req, res) => {
     }
 };
 
+// Login de usuario
 exports.login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = await User.findOne({ email }).populate('school');
+        const user = await User.findOne({ email: email.toString() }).populate('school');
         if (!user) {
             throw new Error('Invalid credentials');
         }
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compare(password.toString(), user.password);
         if (!isMatch) {
             throw new Error('Invalid credentials');
         }
