40ca6a12bb145f55ef6f21eb9df16c65745076f9
IrisCode <150204523+frank777ch@users.noreply.github.com>
2024-07-08 15:52:55 -0500

    Update authController.js
    Fixing to be no vulnerable to injection attacks.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,24 @@ const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// Registro de usuario
 exports.register = async (req, res) => {
     const { firstName, lastName, email, password, birthDate, phone, address, gender, school } = req.body;
     try {
-        const user = new User({ firstName, lastName, email, password, birthDate, phone, address, gender, school });
+        // Sanitizar entradas
+        const sanitizedUser = {
+            firstName: firstName.toString(),
+            lastName: lastName.toString(),
+            email: email.toString(),
+            password: password.toString(),
+            birthDate: new Date(birthDate),
+            phone: phone.toString(),
+            address: address.toString(),
+            gender: gender.toString(),
+            school: school.toString()
+        };
+        
+        const user = new User(sanitizedUser);
         await user.save();
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.status(201).json({ token });
@@ -14,14 +28,18 @@ exports.register = async (req, res) => {
     }
 };
 
+// Login de usuario
 exports.login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = await User.findOne({ email }).populate('school');
+        const sanitizedEmail = email.toString();
+        const sanitizedPassword = password.toString();
+
+        const user = await User.findOne({ email: sanitizedEmail }).populate('school');
         if (!user) {
             throw new Error('Invalid credentials');
         }
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compare(sanitizedPassword, user.password);
         if (!isMatch) {
             throw new Error('Invalid credentials');
         }
